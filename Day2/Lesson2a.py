c3a30a560bad0f58f3652917439068f9d158749b
Imamhasandiit <56116229+Imamhasandiit@users.noreply.github.com>
2019-10-03 17:10:48 +0600

    Add files via upload

---
+++ Day2/Lesson2a.py
@@ -0,0 +1,14 @@
+
+a,b,c = 5, 3.2, "\"Hellow\""
+print(a)
+print(b)
+print(c)
+
+print ("%s\t %s\t %s\t" % (a,b,c))
+print ("%s\n %s\n %s\n" % (a,b,c))
+print (' I love {1} and {0} ' .format('bread','butter'))
+print (' I love {0} and {1} ' .format('bread','butter'))
+
+age = 23
+message="Happy " + str(age) + "rd Birthday!"
+print (message)
---
+++ Day2/lesson2b.py
@@ -0,0 +1,19 @@
+s="hAve a nice DaY"
+print (s)
+
+print (s.upper ())
+print (s.lower ())
+print (s.capitalize ())
+print (s.title ())
+print (s.swapcase ())
+print (s.strip ())
+print (s.center (50))
+print (s.center (50 ,'*'))
+print (s.replace ('nice', 'good'))
+print (s.casefold ())
+print (s.split ())
+print (s.strip ())
+print (help  (s.strip))
+
+#print (dir  (s))
+print (type(s))
